ac09e51e2dd32311c09d3600cb622d94ed64b34f
Vasco Ceti <vasco.ceti@gmail.com>
2024-04-08 10:32:18 +0200

    add metedata

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -4,6 +4,17 @@ import Image from "next/image";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 const MealDetailPage = ({ params }) => {
   const meal = getMeal(params.slug);
 
--- app/meals/layout.js
+++ app/meals/layout.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our vibrant community",
+};
+
 const MealsLayout = ({ children }) => {
   return <div>{children}</div>;
 };
